d1a2c5a55f7004c857c66561559ef70d43194b21
rhdong <rhdong2017@gmail.com>
2021-01-11 20:15:36 +0800

    switch to yapf for google style

--- tools/format.py
+++ tools/format.py
@@ -6,13 +6,15 @@ def check_bash_call(string):
   check_call(["bash", "-c", string])
 
 
-def _run_format_and_flake8():
+def _run_format():
   files_changed = False
 
   try:
-    check_bash_call("yapf  --style google -r ./")
+    check_bash_call(
+        "find . -name '*.py' -print0 | xargs -0 yapf --style=./.style.yapf -dr")
   except CalledProcessError:
-    check_bash_call("yapf --style google -i -r ./")
+    check_bash_call(
+        "find . -name '*.py' -print0 | xargs -0 yapf --style=./.style.yapf -ir")
     files_changed = True
 
   try:
@@ -32,21 +34,17 @@ def _run_format_and_flake8():
   else:
     print("No formatting needed.")
 
-  print("Running flake8.")
-  check_bash_call("flake8")
-  print("Done")
-
   if files_changed:
     exit(1)
 
 
-def run_format_and_flake8():
+def run_format():
   try:
-    _run_format_and_flake8()
+    _run_format()
   except CalledProcessError as error:
     print("Pre-commit returned exit code", error.returncode)
     exit(error.returncode)
 
 
 if __name__ == "__main__":
-  run_format_and_flake8()
+  run_format()
