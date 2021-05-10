ddd5c8b524bac42421179c576da135f405537bfb
tracy01.xie <tracy01.xie@vipshop.com>
2021-05-10 11:53:14 +0800

    change shell suport serving

--- demo/movielens-100k-estimator/chief.sh
+++ demo/movielens-100k-estimator/chief.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 rm -rf ./ckpt
 rm -rf ./export_dir
-sh stop.sh
 
 export TF_CONFIG='{"cluster": {"worker": ["localhost:2222"], "ps": ["localhost:2223"], "chief": ["localhost:2224"]}, "task": {"type": "chief", "index": 0}}'
 python movielens-100k-estimator.py --mode $1
 
 echo "chief ok"
-sh stop.sh
