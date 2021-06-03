1a3be6b278b14c8422dc54fefba726ad0d8f0678
rhdong <rhdong2017@gmail.com>
2021-06-03 16:08:29 +0800

    [CI]change version to 0.2.0

--- tensorflow_recommenders_addons/version.py
+++ tensorflow_recommenders_addons/version.py
@@ -22,8 +22,8 @@ MAX_TF_VERSION = os.getenv("TF_VERSION", "2.4.1")
 
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = "0"
-_MINOR_VERSION = "0"
-_PATCH_VERSION = "1"
+_MINOR_VERSION = "2"
+_PATCH_VERSION = "0"
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
