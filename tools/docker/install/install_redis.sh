7a368d00f8398a5614c37c05ecf443b87992ad87
he.jia <mofhejia@163.com>
2021-07-22 14:34:27 +0800

    fix bug

--- tools/docker/install/install_redis.sh
+++ tools/docker/install/install_redis.sh
@@ -34,7 +34,7 @@ mkdir ${install_dir}/redis-$REDIS_VERSION
 if [[ ! -f "bazel-$REDIS_VERSION.tar.gz" ]]; then
   wget https://github.com/redis/redis/archive/refs/tags/$REDIS_VERSION.tar.gz
 fi
-tar tar -cvxf ${install_dir}/bazel-$REDIS_VERSION.tar.gz -C ${install_dir}/redis-$REDIS_VERSION
+tar -cvxf ${install_dir}/bazel-$REDIS_VERSION.tar.gz -C ${install_dir}/redis-$REDIS_VERSION
 cd ${install_dir}/redis-$REDIS_VERSION
 make -j4
 make PREFIX=${install_dir} install
