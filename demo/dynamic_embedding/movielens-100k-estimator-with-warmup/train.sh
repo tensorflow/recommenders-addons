02c9164e6689025cffc8e13033adb041ed6823c9
Mr.Nineteen <dxwang.bill@gmail.com>
2021-12-29 09:19:45 +0800

    [demo] add movielens-100k-estimator-with-warmup

---
+++ demo/dynamic_embedding/movielens-100k-estimator-with-warmup/train.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rm -rf ./ckpt
+rm -rf ./export_dir
+sh stop.sh
+
+sleep 1
+export TF_CONFIG='{"cluster": {"worker": ["localhost:2222"], "ps": ["localhost:2223"], "chief": ["localhost:2224"]}, "task": {"type": "chief", "index": 0}}'
+python movielens-100k-estimator.py --mode train &
+
+sleep 1
+export TF_CONFIG='{"cluster": {"worker": ["localhost:2222"], "ps": ["localhost:2223"], "chief": ["localhost:2224"]}, "task": {"type": "worker", "index": 0}}'
+python movielens-100k-estimator.py --mode train &
+
+sleep 1
+export TF_CONFIG='{"cluster": {"worker": ["localhost:2222"], "ps": ["localhost:2223"], "chief": ["localhost:2224"]}, "task": {"type": "ps", "index": 0}}'
+python movielens-100k-estimator.py --mode train &
+
+echo "ok"
