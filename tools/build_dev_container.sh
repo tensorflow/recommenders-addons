f4b9c85a287aae284508fa27921b05f776249d7c
rhdong <rhdong2017@gmail.com>
2022-11-02 12:46:14 +0800

    [CI] switch TF version to 2.8.3 and 2.6.3

--- tools/build_dev_container.sh
+++ tools/build_dev_container.sh
@@ -3,7 +3,7 @@
 set -x -e
 docker build \
     -f tools/docker/dev_container.Dockerfile \
-    --build-arg TF_VERSION=2.5.1 \
+    --build-arg TF_VERSION=2.8.3 \
     --build-arg TF_PACKAGE=tensorflow-gpu \
     --build-arg PY_VERSION=$PY_VERSION \
     --build-arg HOROVOD_VERSION="0.23.0" \
