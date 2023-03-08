ea7d9b07b9134ea361cec30ad44bd8d893fe473f
rhdong <rhdong2017@gmail.com>
2023-03-08 08:48:54 +0800

    [CI] update CI docker to ubuntu20.04
    - Simplify the CI pipeline by removing some unnecessary testing items.

--- tools/docker/install/install_deb_packages.sh
+++ tools/docker/install/install_deb_packages.sh
@@ -55,7 +55,7 @@ apt-get install -y --no-install-recommends \
     pkg-config \
     python-dev \
     python-setuptools \
-    python-virtualenv \
+    python3-virtualenv \
     python3-dev \
     python3-setuptools \
     rsync \
