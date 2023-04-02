67189da26b7cb1ddc341c75048d5356849b845cb
rhdong <rhdong2017@gmail.com>
2023-04-02 20:15:19 +0800

    [CI] make the pip deps fixed to some historical version

--- .github/workflows/make_wheel_macOS.sh
+++ .github/workflows/make_wheel_macOS.sh
@@ -3,7 +3,7 @@ set -e -x
 export TF_NEED_CUDA=0
 
 python --version
-python -m pip install --default-timeout=1000 delocate==0.9.1 wheel setuptools tensorflow==$TF_VERSION
+python -m pip install --default-timeout=1000 delocate==0.9.1 wheel==0.37.0 setuptools==50.0.0 tensorflow==$TF_VERSION
 python -m pip install --upgrade protobuf==3.19.6
 
 bash tools/testing/build_and_run_tests.sh
