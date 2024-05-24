fb1ca074d570ef3841559dbab66d64d9e913e922
Julian Qian <julian.qian@gmail.com>
2024-05-24 12:54:31 -0700

    fix gpu_num

--- demo/dynamic_embedding/movielens-1m-keras-with-horovod/start.sh
+++ demo/dynamic_embedding/movielens-1m-keras-with-horovod/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf ./export_dir
-gpu_num = nvidia-smi --query-gpu=name --format=csv,noheader | wc -l
-horovodrun -np $gpu_num python movielens-1m-keras-with-horovod.py --mode="train" --model_dir="./model_dir" --export_dir="./export_dir" 
+gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+export gpu_num
+horovodrun -np $gpu_num python movielens-1m-keras-with-horovod.py --mode="train" --model_dir="./model_dir" --export_dir="./export_dir"
