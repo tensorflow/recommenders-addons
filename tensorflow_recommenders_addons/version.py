5e561df83f77789616324374568fd4988685e988
hrong <hrong@nvidia.com>
2024-05-29 18:57:52 -0700

    [CI] switch the version to 0.7.0 and update readme

--- tensorflow_recommenders_addons/version.py
+++ tensorflow_recommenders_addons/version.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,8 +36,8 @@ else:
 
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = "0"
-_MINOR_VERSION = "6"
-_PATCH_VERSION = "1"
+_MINOR_VERSION = "7"
+_PATCH_VERSION = "0"
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
