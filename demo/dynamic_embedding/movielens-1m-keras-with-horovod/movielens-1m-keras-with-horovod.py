512a62ac0b6481656cdee9b61b3c90a80a3c68e9
MoFHeka <mofhejia@163.com>
2024-06-15 03:35:04 +0800

    [fix] Missing Bucketize class in DE keras horovod demo.

--- demo/dynamic_embedding/movielens-1m-keras-with-horovod/movielens-1m-keras-with-horovod.py
+++ demo/dynamic_embedding/movielens-1m-keras-with-horovod/movielens-1m-keras-with-horovod.py
@@ -184,6 +184,25 @@ def embedding_out_split(embedding_out_concat, input_split_dims):
   return embedding_out
 
 
+class Bucketize(tf.keras.layers.Layer):
+
+  def __init__(self, boundaries, **kwargs):
+    self.boundaries = boundaries
+    super(Bucketize, self).__init__(**kwargs)
+
+  def build(self, input_shape):
+    # Be sure to call this somewhere!
+    super(Bucketize, self).build(input_shape)
+
+  def call(self, x, **kwargs):
+    return tf.raw_ops.Bucketize(input=x, boundaries=self.boundaries)
+
+  def get_config(self,):
+    config = {'boundaries': self.boundaries}
+    base_config = super(Bucketize, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
+
 class ChannelEmbeddingLayers(tf.keras.layers.Layer):
 
   def __init__(self,
