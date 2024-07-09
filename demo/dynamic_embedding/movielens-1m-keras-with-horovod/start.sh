cd7983aa373b730a62736254323796e86320fa11
Julian Qian <julian.qian@gmail.com>
2024-07-09 06:24:03 -0700

    fix start.sh

--- demo/dynamic_embedding/movielens-1m-keras-with-horovod/start.sh
+++ demo/dynamic_embedding/movielens-1m-keras-with-horovod/start.sh
@@ -3,4 +3,4 @@ rm -rf ./export_dir
 gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 export gpu_num
 horovodrun -np $gpu_num python movielens-1m-keras-with-horovod.py --mode="train" --model_dir="./model_dir" --export_dir="./export_dir" \
-  --steps_per_epoch=${1:-20000} --shuffle={2:-True}
+  --steps_per_epoch=${1:-20000} --shuffle=${2:-True}
